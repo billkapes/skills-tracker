c63fa0d60d5214ed31f936106829bf9ea9a0d752
billkapes <kapes15@yahoo.com>
2024-09-30 20:18:07 -0500

    studio work

---
+++ src/test/java/com/launchcode/skills_tracker/SkillsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.skills_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
